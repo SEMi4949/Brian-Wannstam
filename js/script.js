bd6bcbdb77d679bb4566eda82dbdf327b922ffd4
SEMi49 <semi.f49@gmail.com>
2026-03-10 21:10:31 +0100

    tiny change
    refactored all

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// You can add interactive features here, like form validation, smooth scrolling, etc.
+
+// Example of smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
